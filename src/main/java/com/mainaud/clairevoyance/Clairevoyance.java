7e7e9a7d75dbf7a8f59dfc4882c30662e04cfb97
Jérôme Mainaud <jmainaud@ippon.fr>
2016-01-21 19:01:51 +0100

    Use text area

--- src/main/java/com/mainaud/clairevoyance/Clairevoyance.java
+++ src/main/java/com/mainaud/clairevoyance/Clairevoyance.java
@@ -1,19 +1,30 @@
 package com.mainaud.clairevoyance;
 
-import org.fxmisc.richtext.CodeArea;
-import org.fxmisc.richtext.LineNumberFactory;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Collection;
+import java.util.List;
+
+import org.fxmisc.richtext.*;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Clairevoyance extends Application {
 
-    private boolean showLabel = true;
+    private CodeArea codeArea;
+    private StackPane responsePane;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -30,33 +41,62 @@ public class Clairevoyance extends Application {
     private SplitPane createMainPane(Stage primaryStage) {
         SplitPane mainPane = new SplitPane();
 
-        CodeArea codeArea = new CodeArea();
+        codeArea = new CodeArea();
         codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
         mainPane.getItems().add(codeArea);
 
-        StackPane responsePane = new StackPane();
+        responsePane = new StackPane();
         mainPane.getItems().add(responsePane);
 
         mainPane.setDividerPositions(0.25);
 
+        mainPane.setOnKeyPressed(this::handleKeyEvent);
+
         return mainPane;
+    }
+
+    private void handleKeyEvent(KeyEvent e) {
+        if (isCrtlOrMeta(e)) {
+            switch (e.getCode()) {
+                case ENTER:
+                    runCommand();
+                    break;
+                case A:
+                    codeArea.selectAll();
+                    break;
+            }
+        }
+    }
+
+    private boolean isCrtlOrMeta(KeyEvent e) {
+        return e.isControlDown() || e.isMetaDown();
     }
 
     private ToolBar createToolBar(Stage primaryStage) {
         ToolBar toolBar = new ToolBar();
 
         Button runBtn = new Button("Run");
+        runBtn.setOnAction((ActionEvent e) -> runCommand());
         toolBar.getItems().add(runBtn);
 
         Button quitBtn = new Button("Quitter");
-        quitBtn.setOnAction((ActionEvent e) -> {
-            primaryStage.close();
-        });
+        quitBtn.setOnAction((ActionEvent e) -> primaryStage.close());
         toolBar.getItems().add(quitBtn);
         return toolBar;
     }
 
-    public static void main(String[] args) {
+
+
+    private void runCommand() {
+        String text = codeArea.getText().trim();
+
+        TextArea resultTa = new TextArea();
+        resultTa.setText(text);
+        responsePane.getChildren().clear();
+        responsePane.getChildren().add(resultTa);
+    }
+
+    public static void main(String[] args) throws IOException {
         launch(Clairevoyance.class, args);
     }
 }
